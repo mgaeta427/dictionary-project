72654916c4f80f50a4389785847b91e0d5455d74
mgaeta427 <mgaeta427@gmail.com>
2024-07-13 00:11:36 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Hello🙋🏻‍♀️
+      </div>
+        
+  );
+}
+
+export default App;
