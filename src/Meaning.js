cabb3b874640384caf0e319a66eb1f5d721ef661
mgaeta427 <mgaeta427@gmail.com>
2024-07-16 09:52:34 -0500

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,26 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
     <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
-    <div>
-    <p>
-        {props.meaning.definition}
-        <br />
-        <em>{props.meaning.example}</em>
-    </p>
-    </div>
-    </div>
-    
+        <div>
+            <div className="definition">{props.meaning.definition}</div>
+            <div className="example">{props.meaning.example}</div>
+                <em>{props.meaning.example}</em>
+                <Synonyms synonyms={props.meaning.synonyms} />
+                   
+        </div>
+        </div>
+
     );
-    
-        
+
+
 }
-
-
-
-           
-    
-       
-  
